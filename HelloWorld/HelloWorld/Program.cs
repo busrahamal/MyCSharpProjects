757c7c29f15dfb2cae9bc2909e66d8b263a39df4
Büşra Hamal <b.hamal95@gmail.com>
2023-06-21 16:58:21 +0300

    HelloWorld Project Added

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read(); //Read() Enter'e basana kadar, ReadKey() herhangi bir tuşa basana kadar Console ekranını ekranda tutar.
+        }
+    }
+}
