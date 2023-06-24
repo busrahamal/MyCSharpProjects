9dd5cbd684fd73e8e5045ebab901fd5fb2b7178d
Büşra Hamal <b.hamal95@gmail.com>
2023-06-24 16:51:58 +0300

    DikdortgenAlanveCevreHesabiDeveloped

---
+++ DikdortgenAlanveCevreHesabi/DikdortgenAlanveCevreHesabi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenAlanveCevreHesabi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Dikdörtgen Alan ve Çevre Hesaplama Uygulaması ***");
+
+            Console.WriteLine(); //degiskenAdi = Camel, DegiskenAdi = Pascal standardı
+
+            int uzunKenar, kisaKenar, alan, cevre;
+
+            Console.Write("Uzun Kenarı Giriniz: ");
+            uzunKenar = Convert.ToInt32(Console.ReadLine()); //ReadLine string olduğu için Int'e çevirdik
+
+            Console.Write("Kısa Kenarı Giriniz: ");
+            kisaKenar = Convert.ToInt32(Console.ReadLine());
+
+            alan = uzunKenar * kisaKenar;
+            cevre = 2 * (uzunKenar + kisaKenar);
+
+            Console.WriteLine();
+            Console.WriteLine("Uzun kenarı {0}, kısa kenarı {1} olan dikdörtgenin alanı {2}, çevresi ise {3} olarak hesaplanmıştır.", uzunKenar, kisaKenar, alan, cevre);
+
+            Console.Read();
+
+
+        }
+    }
+}
