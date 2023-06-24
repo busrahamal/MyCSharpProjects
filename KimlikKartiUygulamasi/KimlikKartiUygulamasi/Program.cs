ea2a1760b315f4fd3b288448929073b6ed8f7e9a
Büşra Hamal <b.hamal95@gmail.com>
2023-06-24 09:12:01 +0300

    KimlikKartiUygulamasiGelistirildi

---
+++ KimlikKartiUygulamasi/KimlikKartiUygulamasi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KimlikKartiUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** KİMLİK KARTI UYGULAMASI ***");
+            Console.WriteLine();
+            string adi, soyadi, universite, sehir, bolum;
+            Console.Write("Adı: ");
+            adi = Console.ReadLine();
+            Console.Write("Soyadı: ");
+            soyadi = Console.ReadLine();
+            Console.Write("Şehir: ");
+            sehir = Console.ReadLine();
+            Console.Write("Üniversite: ");
+            universite = Console.ReadLine();
+            Console.Write("Bölüm: ");
+            bolum = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Adı {0} Soyadı {1} olan öğrenci, {2} şehrinde bulunan {3} Üniversitesinde {4} Bölümünü okumaktadır.",adi, soyadi, sehir, universite, bolum);
+            Console.Read();
+        }
+    }
+}
