ef8faa7e9235c7e5c5266e1234931cf340d0d08a
busrahamal <80472046+busrahamal@users.noreply.github.com>
2023-06-24 19:51:16 +0300

    Updated Program.cs

--- OgrenciSinavNotUygulamasi/OgrenciSinavNotUygulamasi/Program.cs
+++ OgrenciSinavNotUygulamasi/OgrenciSinavNotUygulamasi/Program.cs
@@ -11,7 +11,8 @@ namespace OgrenciSinavNotUygulamasi
         static void Main(string[] args)
         {
 
-            // Double: Ondalıklı sayılar için kullanılır. Kod kısmında . ile değer girilirken console ekranında değer , ile görünür. Double değer, a = 5, a = 5d, a = 5D şeklinde girilir.
+            // Double: Ondalıklı sayılar için kullanılır. Kod kısmında . ile değer girilirken console ekranında değer , ile görünür. 
+            //Double değer, a = 5, a = 5d, a = 5D şeklinde girilir.
             Console.WriteLine("*** Öğrenci Sınav Not Uygulaması ***");
             Console.WriteLine();
 
