3f0c4aa395017fe68f3a5ae51071e0d2b5aba62d
Büşra Hamal <b.hamal95@gmail.com>
2023-06-29 20:56:24 +0300

    ForDongusuDeveloped

---
+++ ForDongusu/ForDongusu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SifirdanYirmiyeKadarCiftSayilar();
+
+            BirdenYuzeKadarSayilar();
+
+            Console.Read();
+        }
+
+        private static void BirdenYuzeKadarSayilar()
+        {
+            for (int i = 1; i <= 100; i++) // i++ == i = i + 1 == i += 1
+            {
+                Console.WriteLine("{0}. Dünyalı Merhaba. Biz Dostuz.", i);
+            }
+        }
+
+        private static void SifirdanYirmiyeKadarCiftSayilar()
+        {
+            for (int i = 0; i <= 20; i = i + 2) // i + = 2
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
