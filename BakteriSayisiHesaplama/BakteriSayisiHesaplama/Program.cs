59041a8af6c37da92027a6858a7f3f792b0c38c0
Büşra Hamal <b.hamal95@gmail.com>
2023-06-30 16:54:45 +0300

    BakteriSayisiHesaplamaDeveloped

---
+++ BakteriSayisiHesaplama/BakteriSayisiHesaplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakteriSayisiHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             - Bir bakteri türü her saatin sonunda kendini 2'ye bölerek çoğalmaktadır.
+             - Yeni oluşan bakteriler de aynı şekilde çoğalmaktadır.
+             - 24 saatin sonunda bu ortamda kaç tane bakteri olur?
+             
+             
+             */
+            int bakteriSayisi = 1;
+            
+            for(int i = 1; i<=24; i++)
+            {
+                bakteriSayisi = bakteriSayisi * 2;
+                Console.WriteLine("{0}. Saatin Sonundaki Bakteri Sayısı: {1}",i,bakteriSayisi);
+            }
+
+            Console.Read();
+        }
+    }
+}
