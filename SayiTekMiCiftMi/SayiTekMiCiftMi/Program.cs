4190cc3954b4cc89619ea6cbe2fc531208f5b3e9
Büşra Hamal <b.hamal95@gmail.com>
2023-06-30 16:22:58 +0300

    SayiTekMiCiftMiDeveloped

---
+++ SayiTekMiCiftMi/SayiTekMiCiftMi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiTekMiCiftMi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Bir sayı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if(sayi % 2 == 0)
+            {
+                Console.Write("Girmiş olduğunuz {0} sayısı çifttir.", sayi);
+            }
+
+            else
+            {
+                Console.Write("Girmiş olduğunuz {0} sayısı tektir.", sayi);
+            }
+
+            Console.Read();
+        }
+    }
+}
