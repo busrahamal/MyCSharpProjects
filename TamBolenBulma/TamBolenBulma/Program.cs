d91472a25e9cdc104a79700c25b7a353f5f5ea29
Büşra Hamal <b.hamal95@gmail.com>
2023-06-30 16:41:09 +0300

    TamBolenBulmaDeveloped

---
+++ TamBolenBulma/TamBolenBulma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamBolenBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Tam Bölenlerinin Hesaplanmasını İstediğiniz Sayıyı Giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= sayi; i++) // Ctrl + K + D: Gerekli boşlukları ayarlar.
+            {
+                if (sayi % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
