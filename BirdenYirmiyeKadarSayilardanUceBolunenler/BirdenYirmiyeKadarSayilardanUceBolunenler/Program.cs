ad511794f81fc4aab74e495e187255ab7cea75b7
Büşra Hamal <b.hamal95@gmail.com>
2023-06-30 16:30:00 +0300

    BirdenYirmiyeKadarSayilardanUceBolunenlerDeveloped

---
+++ BirdenYirmiyeKadarSayilardanUceBolunenler/BirdenYirmiyeKadarSayilardanUceBolunenler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdenYirmiyeKadarSayilardanUceBolunenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 20; i++)
+            {
+                if(i % 3 == 0 )
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
