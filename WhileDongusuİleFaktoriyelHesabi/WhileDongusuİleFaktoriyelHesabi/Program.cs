788833a56fa81418d7377189d1600c0602e9c886
Büşra Hamal <b.hamal95@gmail.com>
2023-06-30 17:55:40 +0300

    WhileDongusuİleFaktoriyelHesabi

---
+++ WhileDongusuİleFaktoriyelHesabi/WhileDongusuİleFaktoriyelHesabi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDongusuİleFaktoriyelHesabi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int faktoriyeliBulunacakSayi, faktoriyel = 1, i = 1;
+            Console.Write("Faktöriyel Değerini Öğrenmek İstediğiniz Sayıyı Giriniz: ");
+            faktoriyeliBulunacakSayi = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= faktoriyeliBulunacakSayi)
+            {
+                faktoriyel *= i;
+                Console.WriteLine("{0} Sayısının Faktöriyel Değeri: {1}", i, faktoriyel);
+                i++;
+            }
+
+            Console.Read();
+
+
+        }
+    }
+}
