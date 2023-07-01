697ad22ecfaa38ae3ec6de62b9c53d1fbaa4ed1e
Büşra Hamal <b.hamal95@gmail.com>
2023-07-01 21:44:42 +0300

    DiziyeKlavyedenDegerGirisiDeveloped

---
+++ DiziyeKlavyedenDegerGirisi/DiziyeKlavyedenDegerGirisi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziyeKlavyedenDegerGirisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler = new string[5];
+
+            for(int i = 0; i < 5; i++) //değerleri hafızaya aldık
+            {
+                Console.Write("{0}. Şehri Giriniz: ", i+1);
+                sehirler[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            for(int j = 0; j < 5; j++) //hafızaya alınan değerleri ekrana yazdırdık
+            {
+                Console.WriteLine("{0}. Şehir: {1}", j+1, sehirler[j]);
+            }
+
+            Console.Read();
+        }
+    }
+}
