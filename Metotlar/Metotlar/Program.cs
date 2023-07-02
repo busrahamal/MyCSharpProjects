07ec82719cb243608452a2fe4527fc9c702dd69a
Büşra Hamal <b.hamal95@gmail.com>
2023-07-02 17:01:19 +0300

    MetotlarİleİlgiliDetaylar

---
+++ Metotlar/Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* METOTLAR
+             
+             * Belli bir işlemi gerçekleştiren ve geliştiriciyi kod tekrarından kurtaran yapılardır.
+             
+             * Temelde ikiye ayrılır: 
+             
+             * Geriye değer döndüren metotlar
+             * Geriye değer döndürmeyen metotlar
+             
+             * Metotlar () sembolü ile tanımlanırlar. 
+             */
+
+            //-------------------------------------------------------------------
+
+            /* Erişim Belirleyiciler (Modifiers)
+              
+             * Metotlarımıza kimlerin erişim sağlayabileceğinin kontrolünün yapıldığı program komutlarıdır.
+             
+             - Public: Herkese açık.
+             - Private: Sadece içinde bulunduğu sınıftan erişim sağlanır. (Gizli)
+             - Internal: Aynı modül içerisinden erişim sağlanır. (Yalnızca kullandığımız kod bloğu içinde erişim sağlanır.)
+             - Protected: Tanımlandığı sınıf ve miras alınan sınıflardan erişim sağlanır.
+
+              
+             */
+
+            // Static: İçinde bulunduğu sınıftan nesne oluşturulmadan kullanılan yapılardır.
+            // Object Oriented Programming: Class, Object, Inheritance, Encapsulation, Abstraction, Polymorphism, Attribute, Method
+
+            /* Metotların Tanımlanması: erisim_belirleyici_turu static metot_turu metot_adi ()
+            {
+              işlemler
+            }
+            */
+            // Class gördüğümüz anda static'i kaldıracağız.
+
+        }
+    }
+}
