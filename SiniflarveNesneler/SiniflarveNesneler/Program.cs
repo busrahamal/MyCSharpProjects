e49b81ccd342edc27d664f8d940704ff754b0bfa
Büşra Hamal <b.hamal95@gmail.com>
2023-07-16 14:45:56 +0300

    SiniflarveNesnelerİleİlgiliBilgiler

---
+++ SiniflarveNesneler/SiniflarveNesneler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiniflarveNesneler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sınıf: Niteliklerimizi barındıran, kod bloklarını bir arada tutan yapılardır. Programlama dünyası için, böl, parçala ve yönet yaklaşımının oluşmasında kullanılan yapılardır. 
+
+            //Nesne: Sınıfların niteliklerine ulaşabilmemiz için gereken komutlardır. 
+
+            //Örnek: Araba sınıf, markalar nesne. Hayvan sınıf, kuş köpek aslan kedi vs. nesne.
+
+            //Nesne Türetme İşlemi: SınıfAdı NesneAdı = new SınıfAdı();
+        }
+    }
+}
