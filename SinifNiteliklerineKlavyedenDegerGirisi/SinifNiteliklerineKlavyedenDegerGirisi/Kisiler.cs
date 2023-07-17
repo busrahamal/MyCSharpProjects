c7c6428b70d554a274037a8434bc16a827eb61eb
Büşra Hamal <b.hamal95@gmail.com>
2023-07-17 16:28:21 +0300

    SinifNiteliklerineKlavyedenDegerGirisiDeveloped

---
+++ SinifNiteliklerineKlavyedenDegerGirisi/SinifNiteliklerineKlavyedenDegerGirisi/Kisiler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinifNiteliklerineKlavyedenDegerGirisi
+{
+    class Kisiler
+    {
+        public int id;
+        public string ad;
+        public string soyad;
+        public int yas;
+        public string dogumYeri;
+        public string meslek; 
+    }
+}
---
+++ SinifNiteliklerineKlavyedenDegerGirisi/SinifNiteliklerineKlavyedenDegerGirisi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinifNiteliklerineKlavyedenDegerGirisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisiler k = new Kisiler();
+            Console.WriteLine(" ***** Kişi Bilgileri ***** ");
+            Console.WriteLine();
+            Console.Write("Id Giriniz: ");
+            k.id = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Ad Giriniz: ");
+            k.ad = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Soyad Giriniz: ");
+            k.soyad = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Yaş Giriniz: ");
+            k.yas = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Doğum Yeri Giriniz: ");
+            k.dogumYeri = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Meslek Giriniz: ");
+            k.meslek = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("----- Kişi Bilgileriniz -----");
+            Console.WriteLine();
+
+            Console.WriteLine(k.id + " - " + k.ad + " " + k.soyad + ", " + k.yas + ", " + k.dogumYeri + ", " + k.meslek);
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
