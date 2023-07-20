f87c8613ed1b7540b1a6d5b0e9b8af4900f7ca4e
Büşra Hamal <b.hamal95@gmail.com>
2023-07-20 13:35:17 +0300

    KapsullemeİleİlgiliBilgiler

---
+++ Kapsulleme/Kapsulleme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsulleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Kapsülleme (Encapsulation)
+             
+            1- Nesnelerin niteliklerinin bilinçsiz kullanımını önlemek,
+            2- İlgili özelliklerin istenilen değer ya da aralıkta olmasını sağlamak,
+            3- Değişkenlere direkt erişimin önüne geçmek,
+            4- Arka planda kodları gizlemek gibi işlevlere sahip OOP terimidir.  
+            5- Anahtar kelime: property (özellik) 
+
+            Örnek: Şehirlerin sadece harflerden oluşması, sadece büyük ya da küçük harf olması. Öğrencinin notunun 0 ile 100 arasında olması kapsüllemeye örnektir. 
+            6- C#'da imleç hangi satırdaysa o satır seçili sayılır. İmleçin bulunduğu satırda Ctrl + R + E yaptığımızda çıkan ekranda Apply'a tıkladığımızda imlecin bulunduğu satıra ait property oluşur. Property oluştuğunda otomatik olarak get ve set bloklarını oluşturdu. Get bloğuyla ilgili değeri bana geri dönderecek,  set bloğuyla ise varsa kısıtlama uygulayacak. set => ad = value.ToLower(); dediğimiz zaman ne girersek girelim ad her zaman küçük harfli olur.Buradaki value bizim klavyeden gönderdiğimiz değerimizdir. Bunları Class'ımızda gerçekleştiriyoruz.
+             
+             */
+        }
+    }
+}
